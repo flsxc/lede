f531c292b159fc89184ee48baa85f9d39e9cf31d
flsxc <85815460+flsxc@users.noreply.github.com>
2023-04-19 13:47:03 +0800

    Update my.sh

--- doc/my.sh
+++ doc/my.sh
@@ -478,6 +478,8 @@ function remove_trojan(){
         find / | grep nginx | sudo xargs rm -rf
     fi
     rm -rf /usr/src/trojan/
+    rm -rf /usr/src/trojan-cert/
+    rm -rf /usr/src/trojan-temp/
     rm -rf /usr/src/trojan-cli/
     rm -rf /usr/share/nginx/html/*
     rm -rf /etc/nginx/
