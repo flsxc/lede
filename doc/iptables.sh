ea7e850c448a16f69e92d3f88b2e2776b7e274d1
flsxc <85815460+flsxc@users.noreply.github.com>
2023-04-25 02:27:35 +0800

    Update iptables.sh

--- doc/iptables.sh
+++ doc/iptables.sh
@@ -1,2 +1,12 @@
 iptables -t nat -A PREROUTING -i br-lan -p udp --dport 53 -j DNAT --to $(uci get network.lan.ipaddr):53
 iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 53 -j DNAT --to $(uci get network.lan.ipaddr):53
+
+
+
+ 以下规则是屏蔽以 youtube.com 为主的所有一级 二级 三级等域名。
+
+iptables -A OUTPUT -m string --string "youtube.com" --algo bm --to 65535 -j DROP
+ # 添加屏蔽规则
+
+iptables -D OUTPUT -m string --string "youtube.com" --algo bm --to 65535 -j DROP
+ # 删除屏蔽规则，上面添加的代码是什么样，那么删除的代码就是把 -A 改成 -D
