1d093555f7a5f8c2762c66e0925070b0d01d2b7c
flsxc <85815460+flsxc@users.noreply.github.com>
2023-04-26 00:54:14 +0800

    Update iptables.sh

--- doc/iptables.sh
+++ doc/iptables.sh
@@ -1,6 +1,5 @@
-iptables -t nat -A PREROUTING -i br-lan -p udp --dport 53 -j DNAT --to $(uci get network.lan.ipaddr):53
-iptables -t nat -A PREROUTING -i br-lan -p tcp --dport 53 -j DNAT --to $(uci get network.lan.ipaddr):53
 
+iptables -A OUTPUT -m string --string "youtube.com" --algo bm --to 65535 -j DROP
 
 
  以下规则是屏蔽以 youtube.com 为主的所有一级 二级 三级等域名。
